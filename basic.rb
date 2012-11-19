c355133e0347dc057b57c0476100ea1d98c8aec6
Leo Royzengurt <rebellabs@gmail.com>
2012-11-19 16:45:50 -0500

    sinatra_practice
    some sinatra practice inclduing a decrypot and encryptor

---
+++ basic.rb
@@ -0,0 +1,53 @@
+require "rubygems"
+require "sinatra"
+
+get "/" do 
+  "Hello World"
+end
+
+
+get "/about" do
+  "this is a little about me"
+end
+
+get "/lol" do
+  erb :lulzy
+end
+
+post "/lol" do
+  "Your so '#{params[:lulz]}'z"
+end
+
+
+
+get "/hello/:name" do
+  params[:name]
+end
+
+
+
+get "/form" do
+  erb :form
+end
+
+post '/form' do  
+  "You said '#{params[:message]}'"  
+end
+
+get "/secret" do 
+  erb :secret
+end
+
+post '/secret' do  
+  params[:secret].reverse  
+end
+
+get "/decrypt" do
+  erb :decryptor
+end
+
+
+post '/decrypt' do  
+  params[:message_to_decrypt].reverse  
+end
+
